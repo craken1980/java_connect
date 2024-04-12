4689889a60813f2fa9f10f979dddc5b62f75446a
craken1980 <aikido1969@live.com>
2024-04-12 22:55:54 +0200

    Added JDBC conection

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,5 +1,10 @@
 package main;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java_connect.util.MySqlConection;
 import java_connect.vista.*;
 
 public class Main {
@@ -9,32 +14,40 @@ public class Main {
         MenuSocios menuS = new MenuSocios();
         MenuInscripciones menuI = new MenuInscripciones();
         MenuExcursiones menuE = new MenuExcursiones();
-
-        while(true){
+        verificarConexion();
+        while (true) {
             System.out.println("Menu Principal");
             menuP.imprimir();
             int opcionElegida = menuP.elegirOpcion(true);
-            if(opcionElegida == 1){
+            if (opcionElegida == 1) {
                 System.out.println("Menu Excursiones");
                 menuE.imprimir();
                 opcionElegida = menuE.elegirOpcion(true);
                 menuE.getMenu()[opcionElegida - 1].Ejecutar();
                 Menu.enterParaContinuar();
-            }
-            else if(opcionElegida == 2){
+            } else if (opcionElegida == 2) {
                 System.out.println("Menu Socios");
                 menuS.imprimir();
                 opcionElegida = menuS.elegirOpcion(true);
                 menuS.getMenu()[opcionElegida - 1].Ejecutar();
                 Menu.enterParaContinuar();
-            }
-            else if(opcionElegida == 3){
+            } else if (opcionElegida == 3) {
                 System.out.println("Menu Inscripciones");
                 menuI.imprimir();
                 opcionElegida = menuI.elegirOpcion(true);
                 menuI.getMenu()[opcionElegida - 1].Ejecutar();
                 Menu.enterParaContinuar();
             }
+        }
+    }
+
+    private static void verificarConexion() {
+        try {
+            Connection conexion = MySqlConection.getConnection();
+            System.out.println("Conexión exitosa");
+
+        } catch (SQLException ex) {
+            System.out.println("No se pudo establecer conexión con la base de datos");
         }
     }
 
